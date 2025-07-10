88be54c3b44db48963ee4066f6955f2e56d7052f
Gustavo Silva <gustavops1985@gmail.com>
2025-07-10 12:57:39 -0300

    feat: add create question, create room and get room questions

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -7,14 +7,14 @@ await reset(db, schema)
 await seed(db, schema).refine((f) => {
   return {
     rooms: {
-      count: 5,
+      count: 20,
       columns: {
         name: f.companyName(),
         description: f.loremIpsum(),
       },
     },
     questions: {
-      count: 20,
+      count: 20
     },
   }
 })
